d2849427a644f0f6eff5a78b07dd1232fec31bd8
mehdi-benhariz <benharizmehdi20@gmail.com>
2024-12-10 15:01:03 +0100

    modifs

--- obj/Debug/net8.0/projettp.AssemblyInfo.cs
+++ obj/Debug/net8.0/projettp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projettp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a6e68ad38b6b0b193d50851bb9b02b344d8a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f7b5c73ae6eda41eb12b2333a8a91155a288662")]
 [assembly: System.Reflection.AssemblyProductAttribute("projettp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projettp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
